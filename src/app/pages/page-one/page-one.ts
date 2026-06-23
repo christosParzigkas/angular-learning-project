3b330cdedf58485676ca21845e4d547ffaacc56b
christosParzigkas <193087052+christosParzigkas@users.noreply.github.com>
2026-06-24 02:26:44 +0300

    updated app template created pages and added routing (#2)

---
+++ src/app/pages/page-one/page-one.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-one',
+  templateUrl: './page-one.html',
+  styleUrl: './page-one.scss',
+})
+export class PageOne {}
