6997183c8e78203e6286d3c6288e8ba8e3056bc4
Christos Parzigkas <193087052+christosParzigkas@users.noreply.github.com>
2026-06-24 02:24:47 +0300

    updated app template created pages and added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { PageOne } from './pages/page-one/page-one';
+import { PageTwo } from './pages/page-two/page-two';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'page-one', component: PageOne },
+  { path: 'page-two', component: PageTwo },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/app/pages/page-two/page-two.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-two',
+  templateUrl: './page-two.html',
+  styleUrl: './page-two.scss',
+})
+export class PageTwo {}
